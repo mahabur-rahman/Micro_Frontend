a6177b1b3e47a8c23abc8756117e57b77abe3ce3
mahabur-rahman <annur4395@gmail.com>
2024-11-10 21:26:50 +0600

    webpack config file setup

--- products/webpack.config.js
+++ products/webpack.config.js
@@ -1,3 +1,14 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
 export default {
   mode: "development",
+  devServer: {
+    port: 8080,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "index.html", 
+    }),
+  ],
 };
